8b805c2a686ec733801b92c1ef496c34092f0716
francois07 <francois.soulie@edu.esiee.fr>
2021-03-14 18:32:13 +0100

    refactored: clear command

--- src/commands/LobbeeClearCommand.ts
+++ src/commands/LobbeeClearCommand.ts
@@ -9,32 +9,40 @@ export default class LobbeeCommand extends Command {
       channel: "guild",
       description: {
         content:
-          "Use this command if for some reason some empty temporary channels were not deleted",
+          "Use this command if for some reason empty temporary channels were not deleted",
         usage: "lobbeeclear",
         examples: ["lobbeeclear"],
       },
-      clientPermissions: ["MANAGE_CHANNELS", "VIEW_CHANNEL"],
+      clientPermissions: ["MANAGE_CHANNELS", "VIEW_CHANNEL", "SEND_MESSAGES"],
       userPermissions: ["MANAGE_CHANNELS"],
     });
   }
 
   public async exec(message: Message): Promise<MessageEmbed> {
-    const tempChannels = this.client.tempChannels.getAll(message.guild!.id);
+    const tempChannels: Object = await this.client.tempChannels.getAll(
+      message.guild!.id
+    );
     let i = 0;
-    if (tempChannels) {
-      Object.keys(tempChannels).forEach((channelId) => {
-        const channel = message.guild!.channels.resolve(channelId);
-        if (channel && channel.members.size < 1) {
-          channel.delete().catch((e) => {
-            throw e;
-          });
-          i++;
-        } else if (!channel) {
-          this.client.tempChannels.delete(message.guild!.id, channelId);
-          i++;
-        }
-      });
+    if (!tempChannels)
+      throw new Error(
+        "🔍 I couldn't find any empty temporary channel on this server"
+      );
+    const channelIds = Object.keys(tempChannels);
+    for (const id of channelIds) {
+      const channelResolved = message.guild!.channels.resolve(id);
+      if (channelResolved && channelResolved.members.size < 1) {
+        channelResolved.delete().catch((e) => {
+          throw e;
+        });
+        i++;
+      } else if (!channelResolved) {
+        this.client.tempChannels.delete(message.guild!.id, id).catch((e) => {
+          throw e;
+        });
+        i++;
+      }
     }
+
     return new MessageEmbed()
       .setTitle("Cleared")
       .setDescription(`Cleared \`\`${i}\`\` temporary channels`);
