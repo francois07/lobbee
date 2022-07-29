6a79bc29a4b6522eef80d738ee1f77ba9d4589c8
francois07 <francois.soulie@edu.esiee.fr>
2022-07-29 09:18:54 +0200

    Refactored commands

--- src/commands/lobbee.ts
+++ src/commands/lobbee.ts
@@ -1,50 +1,63 @@
 import {
   ChannelType,
   ChatInputCommandInteraction,
+  PermissionFlagsBits,
   SlashCommandBuilder,
 } from "discord.js";
 import { prisma } from "../db";
 
-const LobbeeCommand = {
-  name: "lobbee",
-  builder: new SlashCommandBuilder()
-    .setName("lobbee")
-    .setDescription("Create, update & delete lobbies")
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("create")
-        .setDescription("Defines an existings channel as a lobby creator")
-        .addChannelOption((option) =>
-          option
-            .setName("channel")
-            .setDescription("The channel to define as a lobby creator")
-            .setRequired(true)
-            .addChannelTypes(ChannelType.GuildVoice)
-        )
-        .addStringOption((option) =>
-          option
-            .setName("created_name")
-            .setDescription("The created lobbies' name")
-            .setRequired(true)
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("numbered")
-            .setDescription(
-              "Wether or not the created lobbies should be numbered"
-            )
-        )
-        .addIntegerOption((option) =>
-          option
-            .setName("user_limit")
-            .setDescription("The created lobbies' user limit")
-        )
-    ),
-  run: async (interaction: ChatInputCommandInteraction) => {
-    const { options } = interaction;
+const builder = new SlashCommandBuilder()
+  .setName("lobbee")
+  .setDescription("Create, update & delete lobbies")
+  .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("create")
+      .setDescription("Defines an existings channel as a lobby creator")
+      .addChannelOption((option) =>
+        option
+          .setName("channel")
+          .setDescription("The channel to define as a lobby creator")
+          .setRequired(true)
+          .addChannelTypes(ChannelType.GuildVoice)
+      )
+      .addStringOption((option) =>
+        option
+          .setName("created_name")
+          .setDescription("The created lobbies' name")
+          .setRequired(true)
+      )
+      .addBooleanOption((option) =>
+        option
+          .setName("numbered")
+          .setDescription(
+            "Wether or not the created lobbies should be numbered"
+          )
+      )
+      .addIntegerOption((option) =>
+        option
+          .setName("user_limit")
+          .setDescription("The created lobbies' user limit")
+      )
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("delete")
+      .setDescription("Removes a lobby creator")
+      .addChannelOption((option) =>
+        option
+          .setName("channel")
+          .setDescription("The channel to remove as a lobby creator")
+          .setRequired(true)
+      )
+  )
 
-    switch (options.getSubcommand()) {
-      case "create":
+const run = async (interaction: ChatInputCommandInteraction) => {
+  const { options } = interaction;
+
+  switch (options.getSubcommand()) {
+    case "create":
+      {
         const channel = options.getChannel("channel")!;
         const created_name = options.getString("created_name")!;
         const numbered = options.getBoolean("numbered");
@@ -59,13 +72,30 @@ const LobbeeCommand = {
           },
         });
 
-        interaction.reply({
+        return interaction.reply({
           ephemeral: true,
           content: `\`\`${channel.name}\`\` is now a lobby creator. It will create \`\`${created_name}\`\` lobbies 👌`,
         });
-        break;
-    }
-  },
-};
+      }
+      break;
 
-export default LobbeeCommand;
+    case "delete":
+      {
+        const channel = options.getChannel("channel")!;
+
+        await prisma.channel_creator.deleteMany({
+          where: {
+            channel_discord_id: channel.id,
+          },
+        });
+
+        return interaction.reply({
+          ephemeral: true,
+          content: `\`\`${channel.name}\`\` will not create lobbies anymore 👌`,
+        });
+      }
+      break;
+  }
+}
+
+export { builder, run }
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,21 +1,18 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
-const PingCommand = {
-  name: "ping",
-  builder: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Ping command with a twist !")
-    .addStringOption((option) =>
-      option
-        .setName("input")
-        .setDescription("What the bot will reply to you")
-        .setMaxLength(15)
-    ),
-  run: (interaction: ChatInputCommandInteraction) => {
-    const str = interaction.options.getString("input");
-    console.log(interaction.options.data);
-    interaction.reply({ content: str, ephemeral: true });
-  },
-};
+const builder = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Ping command with a twist !")
+  .addStringOption((option) =>
+    option
+      .setName("input")
+      .setDescription("What the bot will reply to you")
+      .setMaxLength(15)
+  )
 
-export default PingCommand;
+const run = async (interaction: ChatInputCommandInteraction) => {
+  const str = interaction.options.getString("input");
+  return interaction.reply({ content: str, ephemeral: true });
+}
+
+export { builder, run };
