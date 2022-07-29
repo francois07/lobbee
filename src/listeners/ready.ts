ec28837ea5f441f9246e934a6ce896f2df8b04ba
francois07 <francois.soulie@edu.esiee.fr>
2022-07-29 09:18:38 +0200

    Added listeners

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,12 +1,12 @@
-import { IBelListener } from "discord-bel";
+import { createBelListener } from "discord-bel";
 import { redis } from "../db";
 
-const ReadyListener: IBelListener<"ready"> = {
+const ReadyListener = createBelListener({
   name: "ready",
   run: async (client) => {
-    console.log(`Connected as ${client.user?.username}`);
+    console.log(`Connected as ${client.user.username}`);
     await redis.connect().catch((err) => console.error(err));
   },
-};
+});
 
 export default ReadyListener;
--- src/listeners/voiceStateUpdate.ts
+++ src/listeners/voiceStateUpdate.ts
@@ -1,54 +1,56 @@
-import { IBelListener } from "discord-bel";
+import { createBelListener } from "discord-bel";
 import { ChannelType, VoiceState } from "discord.js";
 import { redis, prisma } from "../db";
 
-const voiceStateUpdateListener: IBelListener<"voiceStateUpdate"> = {
-  name: "voiceStateUpdate",
-  run: async (oldState: VoiceState, newState: VoiceState) => {
-    const tempChannels =
-      (await redis
-        .SMEMBERS("temp_channels")
-        .catch((err) => console.error(err))) ?? [];
+const voiceStateUpdateListener = createBelListener(
+  {
+    name: "voiceStateUpdate",
+    run: async (oldState: VoiceState, newState: VoiceState) => {
+      const tempChannels =
+        (await redis
+          .SMEMBERS("temp_channels")
+          .catch((err) => console.error(err))) ?? [];
 
-    /* Handle newState */
-    try {
-      const newChannel = newState.channel;
-      if (newChannel) {
-        const channelCreator = await prisma.channel_creator.findFirst({
-          where: {
-            channel_discord_id: newChannel.id,
-          },
-        });
-
-        if (channelCreator) {
-          const createdChannel = await newChannel.guild.channels.create({
-            name: channelCreator.created_name,
-            type: ChannelType.GuildVoice,
-            parent: newChannel.parent,
+      /* Handle newState */
+      try {
+        const newChannel = newState.channel;
+        if (newChannel) {
+          const channelCreator = await prisma.channel_creator.findFirst({
+            where: {
+              channel_discord_id: newChannel.id,
+            },
           });
 
-          await redis.SADD("temp_channels", createdChannel.id);
-          await newState.setChannel(createdChannel.id);
-        }
-      }
-    } catch (err) {
-      console.error(err);
-    }
+          if (channelCreator) {
+            const createdChannel = await newChannel.guild.channels.create({
+              name: channelCreator.created_name,
+              type: ChannelType.GuildVoice,
+              parent: newChannel.parent,
+            });
 
-    /* Handle oldState */
-    try {
-      const oldChannel = oldState.channel;
-      if (oldChannel) {
-        const isTempChannel = tempChannels.includes(oldChannel.id);
-        if (isTempChannel && oldChannel.members.size < 1) {
-          await oldChannel.delete("Empty temporary channel");
-          await redis.SREM("temp_channels", oldChannel.id);
+            await redis.SADD("temp_channels", createdChannel.id);
+            await newState.setChannel(createdChannel.id);
+          }
         }
+      } catch (err) {
+        console.error(err);
       }
-    } catch (err) {
-      console.error(err);
+
+      /* Handle oldState */
+      try {
+        const oldChannel = oldState.channel;
+        if (oldChannel) {
+          const isTempChannel = tempChannels.includes(oldChannel.id);
+          if (isTempChannel && oldChannel.members.size < 1) {
+            await oldChannel.delete("Empty temporary channel");
+            await redis.SREM("temp_channels", oldChannel.id);
+          }
+        }
+      } catch (err) {
+        console.error(err);
+      }
     }
   },
-};
+);
 
 export default voiceStateUpdateListener;
